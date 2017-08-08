c06a49922393b0f6d322c28d8909207208eb2158
s1523144\1523144 <s_syunsuke0924@icloud.com>
2017-08-08 14:15:12 +0900

    SceneMove

---
+++ Chameleon2/Assets/Scripts/MoveScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+
+	public void SceneManagmentTutorial(){
+		SceneManager.LoadScene ("Tutorial");
+	}
+	public void SceneManagmentMain(){
+		SceneManager.LoadScene ("Main");
+	}
+	public void SceneManagmentResult(){
+		SceneManager.LoadScene ("Result");
+	}
+	public void SceneManagmentTitle(){
+		SceneManager.LoadScene ("Title");
+	}
+}
