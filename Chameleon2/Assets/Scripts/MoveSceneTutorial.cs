e6e88145197de8c28f912e2dd49f1f053ec8512a
s1523144\1523144 <s_syunsuke0924@icloud.com>
2017-08-08 13:40:51 +0900

    no message

--- Chameleon2/Assets/Scripts/MoveSceneTutorial.cs
+++ Chameleon2/Assets/Scripts/MoveSceneTutorial.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MoveSceneTutorial : MonoBehaviour {
 
@@ -12,5 +13,10 @@ public class MoveSceneTutorial : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+
+	}
+
+	void SceneManegment(){
+		SceneManager.LoadScene ("Tutorial");
 	}
 }
