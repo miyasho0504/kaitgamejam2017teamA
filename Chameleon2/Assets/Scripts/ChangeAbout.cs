7b366081c7bd03ec982d511715a332276b8fe7e9
s1523144\1523144 <s_syunsuke0924@icloud.com>
2017-08-09 16:20:21 +0900

    Tutorial

---
+++ Chameleon2/Assets/Scripts/ChangeAbout.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeAbout : MonoBehaviour {
+	public string[] About;
+	public Text uiText;
+	int mojiLine=0;
+
+	// Use this for initialization
+	void Start () {
+		
+		TextUpdate ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision){
+		//ステージ終わりのオブジェクトに触れたら
+		if (collision.gameObject.tag == "About") {
+			
+			TextUpdate ();
+		}
+		if (collision.gameObject.tag == "Restart") {
+			TextUpdate ();
+		}
+
+	}
+
+	void TextUpdate(){
+		uiText.text = About [mojiLine];
+		mojiLine++;
+		if (mojiLine == 2) {
+			mojiLine = 0;
+
+		}
+	}
+
+		
+
+}
