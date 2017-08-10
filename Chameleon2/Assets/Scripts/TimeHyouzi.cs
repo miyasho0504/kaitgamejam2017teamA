43c8393a1e64ab408d3e3ac92ecd03790a14858d
miyasho <miyashi405@gmail.com>
2017-08-10 14:52:12 +0900

    timeScript hensyuu

--- Chameleon2/Assets/Scripts/TimeHyouzi.cs
+++ Chameleon2/Assets/Scripts/TimeHyouzi.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeHyouzi : MonoBehaviour {
+
+    public Text _timeText;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +14,6 @@ public class TimeHyouzi : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        _timeText.text = "Time:" + TimeKeisoku._Time.ToString("f2");
 	}
 }
--- Chameleon2/Assets/Scripts/TimeKeisoku.cs
+++ Chameleon2/Assets/Scripts/TimeKeisoku.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class TimeKeisoku : MonoBehaviour {
 
+    static public float _Time;
+
 	// Use this for initialization
 	void Start () {
-		
+        time_reset();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        time_add();
 	}
+
+    static public void time_add()
+    {
+        _Time += Time.deltaTime;
+    }
+    static public void time_reset()
+    {
+        _Time = 0;
+    }
 }
